bfc1a380ad78f41a28a7185ad42bc8088f4e25f4
Chibo <yurizin163@gmail.com>
2021-12-06 23:14:34 -0300

    ajustes no back-end

--- Back-End/senai_spmedgroup_webAPI/senai_spmedgroup_webAPI/Domains/Consultum.cs
+++ Back-End/senai_spmedgroup_webAPI/senai_spmedgroup_webAPI/Domains/Consultum.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -8,10 +9,20 @@ namespace senai_spmedgroup_webAPI.Domains
     public partial class Consultum
     {
         public int IdConsulta { get; set; }
+
+        [Required(ErrorMessage = "Médico é necessário para agendar uma consulta.")]
         public int? IdMedico { get; set; }
+
+        [Required(ErrorMessage = "Paciente é necessário para agendar uma consulta.")]
         public int? IdPaciente { get; set; }
+
+        [Required(ErrorMessage = "Situação é necessário para agendar uma consulta.")]
         public string Situacao { get; set; }
+
+        [Required(ErrorMessage = "Data é necessário para agendar uma consulta.")]
         public DateTime DataConsulta { get; set; }
+
+        [Required(ErrorMessage = "Valor é necessário para agendar uma consulta.")]
         public decimal? Valor { get; set; }
         public string Descricao { get; set; }
 
