35216b258e1b39be0d644ecfbd421e45abf1d732
Yuri Chiba <yurizin163@gmail.com>
2021-12-08 11:13:01 -0300

    listagem funcionando

--- Mobile/SpMedMobile/App.js
+++ Mobile/SpMedMobile/App.js
@@ -1,97 +1,48 @@
-import axios from 'axios';
-import React, { Component } from 'react';
-import {
-  FlatList,
-  Image,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
+import 'react-native-gesture-handler';
 
-import api from './src/services/api';
+import React, {Component} from 'react';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import {StatusBar, StyleSheet} from 'react-native';
+
+import Main from './src/screens/main';
+import Login from './src/screens/login';
+
+const AuthStack = createStackNavigator();
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      listaConsultas: []
-    };
-  };
-
-  buscarConsultas = async () => {
-    const resposta = await api.get('/consultas');
-    const dadosDaApi = resposta.data;
-    this.setState({ listaConsultas: dadosDaApi });
-  }
-
-  componentDidMount() {
-    this.buscarConsultas();
-  }
-
   render() {
     return (
-      <View style={styles.main}>
-
-        <View style={styles.mainHeader}>
-          <View style={styles.mainHeaderRow}>
-            <Image
-              source={require('./src/assets/img/Imagem_logo.png')}
-              style={styles.mainHeaderRowImg}
-            />
-            <Text style={styles.mainHeaderText}>{"Consultas".toUpperCase()}</Text>
-          </View>
-          <View style={styles.mainHeaderLine} />
-        </View>
-
-        <View style={styles.mainBody}>
-          <FlatList
-            contentContainerStyle={styles.mainBodyContent}
-            data={this.state.listaConsultas}
-            keyExtractor={ item => item.idConsulta }
-            renderItem={ this.renderItem}
-          />
-        </View>
-
-      </View>
+      <NavigationContainer>
+        <StatusBar
+          hidden={true}
+        />
+        <AuthStack.Navigator
+          screenOptions={{
+            headerShown: false,
+          }}>
+          <AuthStack.Screen name="Login" component={Login} />
+          <AuthStack.Screen name="Main" component={Main} />
+        </AuthStack.Navigator>
+      </NavigationContainer>
     );
   }
-
-  renderItem =({ item }) => (
-    // <Text style={{fontSize: 20, color: 'red' }}>{item.idConsulta}</Text>
-    <View style={styles.flatItemRow}>
-      <View style={styles.flatItemContainer}>
-        <Text>{item.idMedico}</Text>
-        <Text>{item.idPaciente}</Text>
-        <Text>{item.dataConsulta}</Text>
-        <Text>-----------------------------------------</Text>
-      </View>
-      <View style={styles.flatItemImg}>
-
-      </View>
-    </View>
-  )
-};
+}
 
 const styles = StyleSheet.create({
+  // conteúdo da main
   main: {
     flex: 1,
-    backgroundColor: 'fff',
+    backgroundColor: '#F1F1F1',
   },
 
-  mainHeader: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-
-  mainHeaderRow:{
-    flexDirection: 'row'
-  },
-
-  mainHeaderRowImg: {
+  // estilo dos ícones da tabBar
+  tabBarIcon: {
     width: 22,
-    height: 22
-  }
+    height: 22,
+  },
 });
 
 export default App;
